8d46dc43b3a2523e1a8f96632bb6a9308eef43f5
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2023-07-10 19:47:36 +0900

    Add Gessetti.tet

--- script.rb
+++ script.rb
@@ -1,10 +1,12 @@
 # Godotの画面上でテーマを「Custom」にして色をいじると、
-# ~/Library/Application Support/Godot/editor_settings-4.tres
+# ~/Library/Application\ Support/Godot/editor_settings-4.tres
 # に、
 # text_editor/theme/highlighting/control_flow_keyword_color = Color(0.168627, 0, 0.992157, 1)
 # のような形で格納されていた。
 # "text_editor/theme/" 以下だけ抜き出すのはテキストエディタで簡単にできたが、
 # "Color(1, 1, 1, 1)" のところは "#FFFFFFFF" のように16進数表記にしないといけないらしかったので以下スクリプトで変換した。
+
+require 'active_support/all'
 
 path = "~/path/to/Custom.tet.backup"
 
